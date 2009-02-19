ece39303010d55e8d80d2df773ff83f15dd8fc3c
jyasskin <jyasskin@05521daa-c0b4-11dd-bb00-bd6ab96fe29a>
2009-02-19 17:48:06 +0000

    Undo the default recursion limit change. Removing most superinstructions in r144 seems to have brought the frame size back under control. This fixes `regrtest.py -uall -v test_compiler` without breaking any of the exception tests, in both debug and optimized modes. I may or may not have tested all of our compilers, though, so let me know if you do get stack overflows.
    git-svn-id: http://unladen-swallow.googlecode.com/svn/trunk@177 05521daa-c0b4-11dd-bb00-bd6ab96fe29a

--- Python/ceval.c
+++ Python/ceval.c
@@ -440,7 +440,7 @@ Py_MakePendingCalls(void)
 /* The interpreter's recursion limit */
 
 #ifndef Py_DEFAULT_RECURSION_LIMIT
-#define Py_DEFAULT_RECURSION_LIMIT 500
+#define Py_DEFAULT_RECURSION_LIMIT 1000
 #endif
 static int recursion_limit = Py_DEFAULT_RECURSION_LIMIT;
 int _Py_CheckRecursionLimit = Py_DEFAULT_RECURSION_LIMIT;
