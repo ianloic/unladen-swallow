935036c249749a4466317d3281174622575547fe
collinw <collinw@05521daa-c0b4-11dd-bb00-bd6ab96fe29a>
2009-03-28 15:35:32 +0000

    Add cross-version tests for cPickle.
    Test pickle backwards compatibility by bouncing pickled objects off through another Python process. This isn't perfect, but it's a decent start.
    
    This includes tests for Python 2.3, 2.4 and 2.5. If you don't have one of those installed, the tests are skipped.
    
    
    git-svn-id: http://unladen-swallow.googlecode.com/svn/trunk@363 05521daa-c0b4-11dd-bb00-bd6ab96fe29a

--- Lib/test/test_xpickle.py
+++ Lib/test/test_xpickle.py
@@ -3,8 +3,16 @@
 # This test covers the other two cases, making pickles with one module and
 # loading them via the other.
 
-import pickle
 import cPickle
+import os
+import pickle
+import sys
+import unittest
+
+# Python 2.3 doesn't have this, but the worker doesn't need it anyway.
+# This is just here to make the worker execute cleanly under 2.3.
+if sys.version_info >= (2, 4):
+    import subprocess
 
 from test import test_support
 from test.pickletester import AbstractPickleTests
@@ -13,7 +21,7 @@ class DumpCPickle_LoadPickle(AbstractPickleTests):
 
     error = KeyError
 
-    def dumps(self, arg, proto=0, fast=0):
+    def dumps(self, arg, proto=0, fast=False):
         # Ignore fast
         return cPickle.dumps(arg, proto)
 
@@ -25,7 +33,7 @@ class DumpPickle_LoadCPickle(AbstractPickleTests):
 
     error = cPickle.BadPickleGet
 
-    def dumps(self, arg, proto=0, fast=0):
+    def dumps(self, arg, proto=0, fast=False):
         # Ignore fast
         return pickle.dumps(arg, proto)
 
@@ -33,11 +41,131 @@ class DumpPickle_LoadCPickle(AbstractPickleTests):
         # Ignore fast
         return cPickle.loads(buf)
 
+def have_python_version(name):
+    """Check whether the given name is a valid Python binary.
+
+    This respects your PATH.
+
+    Args:
+        name: short string name of a Python binary such as "python2.4".
+
+    Returns:
+        True if the name is valid, False otherwise.
+    """
+    # Written like this for Python 2.3 compat.
+    return os.system(name + " -c 'import sys; sys.exit()'") == 0
+
+def send_to_worker(python, obj, proto):
+    """Bounce an object through another version of Python using cPickle.
+
+    This will pickle the object, send it to a child process where it will be
+    unpickled, then repickled and sent back to the parent process.
+
+    Args:
+        python: the name of the Python binary to start.
+        obj: object to pickle.
+        proto: pickle protocol number to use.
+
+    Returns:
+        The pickled data received from the child process.
+    """
+    data = cPickle.dumps((proto, obj), proto)
+    worker = subprocess.Popen([python, __file__, "worker"],
+                              stdin=subprocess.PIPE,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
+    stdout, stderr = worker.communicate(data)
+    if worker.returncode != 0:
+        raise RuntimeError(stderr)
+    return stdout
+
+
+class AbstractCompatTests(AbstractPickleTests):
+
+    python = None
+    error = cPickle.BadPickleGet
+
+    def setUp(self):
+        self.assertTrue(self.python)
+
+    def dumps(self, arg, proto=0, fast=False):
+        return send_to_worker(self.python, arg, proto)
+
+    def loads(self, input):
+        return cPickle.loads(input)
+
+    # These tests are disabled because they require some special setup
+    # on the worker that's hard to keep in sync.
+    def test_global_ext1(self):
+        pass
+
+    def test_global_ext2(self):
+        pass
+
+    def test_global_ext4(self):
+        pass
+
+
+if not have_python_version("python2.3"):
+    class Python23Compat(unittest.TestCase):
+        pass
+else:
+    class Python23Compat(AbstractCompatTests):
+
+        python = "python2.3"
+
+        # Disable these tests for Python 2.3. Making them pass would require
+        # nontrivially monkeypatching the pickletester module in the worker.
+        def test_reduce_calls_base(self):
+            pass
+
+        def test_reduce_ex_calls_base(self):
+            pass
+
+
+if not have_python_version("python2.4"):
+    class Python24Compat(unittest.TestCase):
+        pass
+else:
+    class Python24Compat(AbstractCompatTests):
+
+        python = "python2.4"
+
+        # Disable these tests for Python 2.4. Making them pass would require
+        # nontrivially monkeypatching the pickletester module in the worker.
+        def test_reduce_calls_base(self):
+            pass
+
+        def test_reduce_ex_calls_base(self):
+            pass
+
+
+if not have_python_version("python2.5"):
+    class Python25Compat(unittest.TestCase):
+        pass
+else:
+    class Python25Compat(AbstractCompatTests):
+
+        python = "python2.5"
+
+
+def worker_main(in_stream, out_stream):
+    message = cPickle.load(in_stream)
+    protocol, obj = message
+    cPickle.dump(obj, out_stream, protocol)
+
+
 def test_main():
     test_support.run_unittest(
         DumpCPickle_LoadPickle,
-        DumpPickle_LoadCPickle
+        DumpPickle_LoadCPickle,
+        Python23Compat,
+        Python24Compat,
+        Python25Compat,
     )
 
 if __name__ == "__main__":
-    test_main()
+    if "worker" in sys.argv:
+        worker_main(sys.stdin, sys.stdout)
+    else:
+        test_main()
