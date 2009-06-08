251b4d3ec3921728c7edc698458a28ebf0f00514
collinw <collinw@05521daa-c0b4-11dd-bb00-bd6ab96fe29a>
2009-06-08 17:50:43 +0000

    Update the MSVC project to use #include rather than concatenation for initial_llvm_module.cc and to reference SingleFunctionInliner.{cc|h}.
    Patch by James Abbatiello!
    
    
    git-svn-id: http://unladen-swallow.googlecode.com/svn/trunk@614 05521daa-c0b4-11dd-bb00-bd6ab96fe29a

--- PCbuild/build_clang.bat
+++ PCbuild/build_clang.bat
@@ -2,11 +2,9 @@
 
 set CONFIGURATION=%1
 set PLATFORM=%2
-set INPUTDIR=%3
-set INPUT=%4
-set BITCODE=%5
-set OUTPUT=%6
-set INTDIRDEP=%7
+set INPUT=%3
+set BITCODE=%4
+set OUTPUT=%5
 
 set CLANG="%CD%\..\Util\llvm\obj\bin\Release\clang"
 set LLC="%CD%\..\Util\llvm\obj\bin\Release\llc"
@@ -16,21 +14,9 @@ if "%CONFIGURATION%"=="Debug" set CFLAGS=%CFLAGS% -D_DEBUG
 if "%PLATFORM%"=="x64" set CFLAGS=%CFLAGS% -D_WIN64
 for /F "delims=;" %%I in ("%INCLUDE%") do set CFLAGS=%CFLAGS% -I"%%I"
 
-cd %INPUTDIR%
-if ERRORLEVEL 1 goto end
-
 %CLANG% -O3 -emit-llvm -c %CFLAGS% %INPUT% -o %BITCODE%
 if ERRORLEVEL 1 goto end
 
-%LLC% %BITCODE% -march=cpp -cppgen=contents -cppfname=FillInitialGlobalModule -o %OUTPUT%.tmp -f
-if ERRORLEVEL 1 goto end
-
-copy /B %OUTPUT%.header + %OUTPUT%.tmp %OUTPUT%
-if ERRORLEVEL 1 goto end
-
-del %OUTPUT%.tmp 2> NUL
-
-echo Last built for %CONFIGURATION%^|%PLATFORM%> %OUTPUT%.dep
-echo.> %INTDIRDEP%
+%LLC% %BITCODE% -march=cpp -cppgen=contents -cppfname=FillInitialGlobalModule -o %OUTPUT% -f
 
 :end
