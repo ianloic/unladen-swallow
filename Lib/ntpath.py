21d9dda8aa60b1e3e4caebc8f07ee1791dd13c42
abbeyj <abbeyj@05521daa-c0b4-11dd-bb00-bd6ab96fe29a>
2009-07-02 19:28:57 +0000

    Delay import in ntpath.py to improve startup time (see r675)
    git-svn-id: http://unladen-swallow.googlecode.com/svn/trunk@688 05521daa-c0b4-11dd-bb00-bd6ab96fe29a

--- Lib/ntpath.py
+++ Lib/ntpath.py
@@ -9,7 +9,6 @@ import os
 import sys
 import stat
 import genericpath
-import warnings
 
 from genericpath import *
 
@@ -250,6 +249,7 @@ def walk(top, func, arg):
     beyond that arg is always passed to func.  It can be used, e.g., to pass
     a filename pattern, or a mutable object designed to accumulate
     statistics.  Passing None for arg is common."""
+    import warnings
     warnings.warnpy3k("In 3.x, os.path.walk is removed in favor of os.walk.")
     try:
         names = os.listdir(top)
