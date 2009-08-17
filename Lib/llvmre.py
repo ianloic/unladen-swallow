3400b37176912d6d84eba771b5c5b0e08211b037
Ian McKellar <ian@mckellar.org>
2009-08-17 20:07:05 +0100

    implement llvmre.sub, MatchObject.expand. use NotImplementedError

--- Lib/llvmre.py
+++ Lib/llvmre.py
@@ -11,17 +11,17 @@ class RegexObject(object):
   def __init__(self, pattern, flags):
     # call sre_parse to parse re string into an object
     from sre_parse import parse
-    parsed = parse(pattern, flags)
-    flags = parsed.pattern.flags
+    self.__parsed = parse(pattern, flags)
+    flags = self.__parsed.pattern.flags
     # flatten the subpatterns in the sequence
-    processed = self.__flatten_subpatterns(parsed)
+    processed = self.__flatten_subpatterns(self.__parsed)
     # compile to native code
     from _llvmre import RegEx
-    self.__re = RegEx(processed, flags, parsed.pattern.groups-1)
+    self.__re = RegEx(processed, flags, self.__parsed.pattern.groups-1)
     # expected properties
     self.flags = flags
-    self.groups = parsed.pattern.groups
-    self.groupindex = parsed.pattern.groupdict
+    self.groups = self.__parsed.pattern.groups
+    self.groupindex = self.__parsed.pattern.groupdict
     self.pattern = pattern
 
   def __flatten_subpatterns(self, pattern):
@@ -46,33 +46,34 @@ class RegexObject(object):
     else: _endpos = len(string)
     groups = self.__re.match(unicode(string), _pos, _endpos)
     if groups:
-      return MatchObject(self, string, pos, endpos, groups)
+      return MatchObject(self, string, pos, endpos, groups, self.__parsed)
     else:
       return None
 
   def search(self, string, pos=0, endpos=-1):
-    pass
+    raise NotImplementedError('RegexObject.search')
 
   def split(self, string, maxsplit=0):
-    pass
+    raise NotImplementedError('RegexObject.split')
 
   def findall(self, string, pos=0, endpos=-1):
-    pass
+    raise NotImplementedError('RegexObject.findall')
 
   def finditer(self, string, pos=0, endpos=-1):
-    pass
+    raise NotImplementedError('RegexObject.finditer')
 
   def sub(self, repl, string, count=0):
-    pass
+    raise NotImplementedError('RegexObject.sub')
 
   def subn(self, repl, string, count=0):
-    pass
+    raise NotImplementedError('RegexObject.subn')
 
 
 class MatchObject(object):
-  def __init__(self, regex, string, pos, endpos, groups):
+  def __init__(self, regex, string, pos, endpos, groups, parsed):
     self.__regex = regex
     self.__groups = []
+    self.__parsed = parsed
     while len(groups): self.__groups.append((groups.pop(0), groups.pop(0)))
     # FIXME: implement lastindex, lastgroup
     self.pos = pos
@@ -124,12 +125,15 @@ class MatchObject(object):
     return result
 
   def expand(self, template):
-    # see re._expand
-    raise NotImplementedError('MatchObject.expand')
-
+    # reuse SRE's template code
+    from sre_parse import parse_template, expand_template
+    return expand_template(parse_template(template, self.__parsed), self)
 
 def compile(pattern, flags=0):
   return RegexObject(pattern, flags)
 
 def match(pattern, string, flags=0):
   return compile(pattern, flags).match(string)
+
+def sub(pattern, repl, string, count=0):
+  return compile(pattern, flags).sub(repl, string, count)
